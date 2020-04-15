fff57176391191caf9752b12b97b6cae96ccaaad
Neil Empey <argus.two.2@gmail.com>
2020-04-15 17:20:39 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,21 +1,20 @@
 require 'pry'
 
 def my_find(collection)
-  if(collection.length == nil)
+  if(collection.length == 0)
     return nil 
   end 
   solution = []
-  i = 1 
-  while i < 100 
-  value = yield(i) 
-  if(value == true)
-    solution << i
-  end
+  i = 0 
+  while i < collection.length 
+  value = collection[i]
+  puts value 
+  tf = yield(value.to_i)
+  if(tf == true)
+    solution << value.to_i
+  end 
   i += 1 
 end 
-  if(solution.empty?)
-    return nil 
-  else 
-    return solution.join(' ').
-  end
+return solution.join(' ')
+
 end
