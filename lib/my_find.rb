d1d19d4fe8858df18cba58e20fadf7920132a696
Neil Empey <argus.two.2@gmail.com>
2020-04-15 17:29:01 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,24 +1,6 @@
 require 'pry'
 
 def my_find(collection)
-  if(collection.length == 0)
-    return nil 
-  end 
-  solution = []
-  i = 0 
-  while i < collection.length 
-  value = collection[i]
-  puts value 
-  tf = yield(value.to_i)
-  if(tf == true)
-    solution << value.to_i
-  end 
-  i += 1 
-end 
-if(solution.empty?)
-  return nil 
-else 
-  return solution 
-end
+puts collection 
 
 end
