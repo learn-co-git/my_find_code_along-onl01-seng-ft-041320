8144fbffccc7b77d7ce55c39632210c8bdea41ce
Neil Empey <argus.two.2@gmail.com>
2020-04-15 17:03:08 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
-
+  solution = []
+  i = 0 
+  while i < collection.length 
+  value = yield(collection[i]) 
+  if(value == true)
+    solution << value 
+  end
+  i += 1 
+end 
+  solution
 end
